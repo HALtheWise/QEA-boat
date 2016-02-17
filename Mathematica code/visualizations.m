f6fee430f8bde1f4ca54a20bcab0141a11babc58
Eric Miller <eric@legoaces.org>
2016-02-17 09:53:57 -0500

    Allowed visualizations to handle angled waterlines

--- Mathematica code/visualizations.m
+++ Mathematica code/visualizations.m
@@ -1,10 +1,9 @@
 (* ::Package:: *)
 
-(* Warning: This does not plot the normal properly yet. *)
 showWaterline[region_,normal_,waterHeight_]:=
 Module[{graphics,height},
 graphics=GraphicsComplex[MeshCoordinates@region,MeshCells[region,2]];
 height=waterline[region, normal,waterHeight];
 bounds=RegionBounds[region];
-Show[Graphics3D[{EdgeForm[],Gray,graphics}],Plot3D[height,{x,bounds[[1,1]],bounds[[1,2]]},{y,bounds[[2,1]],bounds[[2,2]]}]]
+Show[Graphics3D[{EdgeForm[],Gray,graphics}],Graphics3D[{Blue,Opacity[0.5],HalfSpace[normal,height]}]]
 ]

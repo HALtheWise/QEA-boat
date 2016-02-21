80d11ec040e7716b9d93fa968a83008d6706238b
jaredbriskman <jared.briskman@students.olin.edu>
2016-02-21 00:18:57 -0500

    more boats, also changing trim angle code in main

--- Mathematica code/boats.m
+++ Mathematica code/boats.m
@@ -48,3 +48,12 @@ masses -> {96.2,364,364},
 region ->  Import["Sample Data/bestboatdisplacement.stl","BoundaryMeshRegion"],
 graphics :> Show[Graphics3D[{EdgeForm[],Gray,GraphicsComplex[MeshCoordinates@region/.boat,MeshCells[region/.boat,2]]}],Graphics3D[{Red,PointSize[4],Graphics3D@Point[massPts/.boat]}]]
 |>;
+
+
+besterboat=<|
+name -> "besterboat",
+massPts (*mast,can1,can2,centered around mast  *)-> {{0,0,20},{7,0,0},{-7,0,0}},
+masses -> {96.2,364,364},
+region ->  Import["Sample Data/besterboat10tall5deg.stl","BoundaryMeshRegion"],
+graphics :> Show[Graphics3D[{EdgeForm[],Gray,GraphicsComplex[MeshCoordinates@region/.boat,MeshCells[region/.boat,2]]}],Graphics3D[{Red,PointSize[4],Graphics3D@Point[massPts/.boat]}]]
+|>;
